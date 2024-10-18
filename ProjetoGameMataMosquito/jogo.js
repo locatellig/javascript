4b7d9146dbcb12a54c92596c2980ede6bb6afbdc
locatellig_notebook <guilhermeloka12@gmail.com>
2024-10-17 21:59:02 -0300

    Javascript Game Mata Mosquito - Parte 01

---
+++ ProjetoGameMataMosquito/jogo.js
@@ -0,0 +1,11 @@
+var alturaDisponivel = 0;
+var larguraDisponivel = 0;
+
+//Variáveis com valores constantemente atualizados
+function ajustaTamanhoPalcoJogo() {
+    alturaDisponivel = window.innerHeight;
+    larguraDisponivel = window.innerWidth;
+
+    console.log(alturaDisponivel, larguraDisponivel);
+}
+
